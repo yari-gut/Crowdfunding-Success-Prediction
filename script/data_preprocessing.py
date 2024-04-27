6a8f904e366adeafd41af90f190c8c0c81c2623d
Yareli Gutiérrez Martinez <116047643+yari-gut@users.noreply.github.com>
2024-04-27 16:00:03 -0400

    Update data_preprocessing.py

--- script/data_preprocessing.py
+++ script/data_preprocessing.py
@@ -1,140 +1,114 @@
+import os
 import pandas as pd
+from sklearn.feature_extraction import FeatureHasher
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
 
-def preprocess_kickstarter_data(kickstarter_data):
+def preprocess_kickstarter(kickstarter_df):
     """
     Preprocess the Kickstarter dataset.
     
     Parameters:
-    - kickstarter_data (DataFrame): DataFrame containing the Kickstarter dataset.
+    - kickstarter_df (DataFrame): DataFrame containing the raw Kickstarter dataset.
     
     Returns:
-    - DataFrame: Preprocessed Kickstarter dataset.
+    - cleaned_df (DataFrame): DataFrame containing the preprocessed Kickstarter dataset.
     """
-    # Drop unnecessary columns
-    kickstarter_data.drop(['ID', 'Subcategory', 'Country'], axis=1, inplace=True)
-    
-    # Convert 'Launched' and 'Deadline' columns to datetime
-    kickstarter_data['Launched'] = pd.to_datetime(kickstarter_data['Launched'])
-    kickstarter_data['Deadline'] = pd.to_datetime(kickstarter_data['Deadline'])
-    
-    # Extract relevant information from datetime columns
-    kickstarter_data['Campaign_Duration'] = (kickstarter_data['Deadline'] - kickstarter_data['Launched']).dt.days
-    
-    # Convert 'State' column to binary: Successful (1) or Failed (0)
-    kickstarter_data['State'] = (kickstarter_data['State'] == 'successful').astype(int)
-    
-    # Normalize numerical features: 'Goal', 'Pledged', 'Backers'
-    scaler = MinMaxScaler()
-    kickstarter_data[['Goal', 'Pledged', 'Backers']] = scaler.fit_transform(kickstarter_data[['Goal', 'Pledged', 'Backers']])
-    
-    # Drop rows with missing values (if necessary)
-    kickstarter_data.dropna(inplace=True)
-    
-    return kickstarter_data
+    # Drop samples with missing values for any feature
+    cleaned_df = kickstarter_df.dropna()
 
-def preprocess_indiegogo_data(indiegogo_data):
+    # Drop features that are not meaningful
+    cleaned_df = cleaned_df.drop(labels=["ID"], axis=1)
+
+    # Convert features related to date and time to POSIX timestamps
+    datetime_features = ["Launched", "Deadline"]
+    for feature in datetime_features:
+        cleaned_df[feature] = pd.to_datetime(cleaned_df[feature]).astype('int64') // 10**9
+    
+    # Convert "Name" feature to numerical using feature hashing
+    cleaned_df["Name"] = cleaned_df["Name"].str.split()
+    hasher = FeatureHasher(n_features=6, input_type="string")
+    hashed_features = hasher.transform(cleaned_df["Name"])
+    hashed_df = pd.DataFrame(hashed_features.toarray(), columns=[f'Name_{i}' for i in range(6)])
+    cleaned_df = pd.concat([cleaned_df, hashed_df], axis=1)
+    cleaned_df = cleaned_df.drop(labels=["Name"], axis=1)
+
+    # Convert categorical columns to numerical using label encoding
+    categorical_features = ["Category", "Subcategory", "Country"]
+    label_encoder = LabelEncoder()
+    for feature in categorical_features:
+        cleaned_df[feature] = label_encoder.fit_transform(cleaned_df[feature])
+
+    # Normalize numeric features
+    numeric_features = ["Launched", "Deadline", "Goal", "Pledged", "Backers"]
+    scaler = MinMaxScaler()
+    cleaned_df[numeric_features] = scaler.fit_transform(cleaned_df[numeric_features])
+
+    # Convert the target column "State" to binary, dropping samples that are not "Successful" or "Failed"
+    cleaned_df = cleaned_df[cleaned_df["State"].isin(['Successful', 'Failed'])]
+    cleaned_df["State"] = cleaned_df["State"].map({"Successful": 1, "Failed": 0})
+
+    return cleaned_df
+
+def preprocess_indiegogo(indiegogo_df):
     """
     Preprocess the Indiegogo dataset.
     
     Parameters:
-    - indiegogo_data (DataFrame): DataFrame containing the Indiegogo dataset.
+    - indiegogo_df (DataFrame): DataFrame containing the raw Indiegogo dataset.
     
     Returns:
-    - DataFrame: Preprocessed Indiegogo dataset.
+    - cleaned_df (DataFrame): DataFrame containing the preprocessed Indiegogo dataset.
     """
-    # Drop unnecessary columns
-    indiegogo_data.drop(['currency', 'year_end', 'month_end', 'day_end', 'time_end', 'project_id', 'tagline', 'url', 'date_launch', 'date_end'], axis=1, inplace=True)
-    
-    # Convert 'year_launch', 'month_launch', 'day_launch', 'time_launch' columns to string
-    indiegogo_data['year_launch'] = indiegogo_data['year_launch'].astype(str)
-    indiegogo_data['month_launch'] = indiegogo_data['month_launch'].astype(str)
-    indiegogo_data['day_launch'] = indiegogo_data['day_launch'].astype(str)
-    indiegogo_data['time_launch'] = indiegogo_data['time_launch'].astype(str)
+    # Drop samples with missing values for any feature
+    cleaned_df = indiegogo_df.dropna()
 
-    # Concatenate and convert to datetime
-    indiegogo_data['year_launch'] = pd.to_datetime(indiegogo_data['year_launch'] + '-' + indiegogo_data['month_launch'] + '-' + indiegogo_data['day_launch'] + ' ' + indiegogo_data['time_launch'])
+    # Drop features that are not meaningful or redundant
+    cleaned_df = cleaned_df.drop(labels=["project_id", "url", "amount_raised", "funded_percent", "category", "tperiod"], axis=1)
 
-    # Drop 'month_launch', 'day_launch', 'time_launch' columns after converting to datetime
-    indiegogo_data.drop(['month_launch', 'day_launch', 'time_launch'], axis=1, inplace=True)
-    
-    # Normalize numerical features: 'amount_raised_usd', 'goal_usd'
+    # Create columns "launched" and "deadline" to condense columns related to date and time
+    cleaned_df["launched"] = pd.to_datetime(cleaned_df['date_launch'] + " " + cleaned_df['time_launch']).astype('int64') // 10**9
+    cleaned_df["end"] = pd.to_datetime(cleaned_df['date_end'] + " " + cleaned_df['time_end']).astype('int64') // 10**9
+    condensed_features = ["year_launch", "month_launch", "day_launch", "time_launch", "year_end", "month_end", "day_end", "time_end", "date_launch", "date_end",
+                          "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+    cleaned_df = cleaned_df.drop(labels=condensed_features, axis=1)
+
+    # Convert features related to descriptive text to numerical using feature hashing
+    text_features = ["title", "tagline"]
+    hashers = {feature: FeatureHasher(n_features=6, input_type='string') for feature in text_features}
+    hashed_dfs = []
+    for feature in text_features:
+        cleaned_df[feature] = cleaned_df[feature].str.split()
+        hashed_features = hashers[feature].fit_transform(cleaned_df[feature])
+        hashed_df = pd.DataFrame(hashed_features.toarray(), columns=[f'{feature}_{i}' for i in range(6)])
+        hashed_dfs.append(hashed_df)
+        cleaned_df.reset_index(drop=True, inplace=True)
+        hashed_df.reset_index(drop=True, inplace=True)
+        cleaned_df = pd.concat([cleaned_df, hashed_df], axis=1)
+    cleaned_df = cleaned_df.drop(labels=text_features, axis=1)
+
+    # Convert categorical features to numerical using one-hot encoding
+    cleaned_df = pd.get_dummies(cleaned_df, columns=["currency"])
+
+    # Normalize numeric features
+    numeric_features = ["launched", "end", "amount_raised_usd", "goal_usd"]
     scaler = MinMaxScaler()
-    indiegogo_data[['amount_raised_usd', 'goal_usd']] = scaler.fit_transform(indiegogo_data[['amount_raised_usd', 'goal_usd']])
-    
-    # Drop rows with missing values (if necessary)
-    indiegogo_data.dropna(inplace=True)
-    
-    return indiegogo_data
+    cleaned_df[numeric_features] = scaler.fit_transform(cleaned_df[numeric_features])
 
-def align_column_names(indiegogo_data, kickstarter_data):
-    """
-    Align column names between Indiegogo and Kickstarter datasets.
-
-    Parameters:
-    - indiegogo_data (DataFrame): DataFrame containing the Indiegogo dataset.
-    - kickstarter_data (DataFrame): DataFrame containing the Kickstarter dataset.
-
-    Returns:
-    - DataFrame: Indiegogo dataset with aligned column names.
-    - DataFrame: Kickstarter dataset with aligned column names.
-    """
-    # Dictionary to map Indiegogo column names to Kickstarter column names
-    indiegogo_to_kickstarter_mapping = {
-        'category': 'Category',
-        'amount_raised_usd': 'Pledged',
-        'goal_usd': 'Goal'
-        # Add more mappings as needed
-    }
-
-    # Rename columns in Indiegogo dataset
-    indiegogo_data.rename(columns=indiegogo_to_kickstarter_mapping, inplace=True)
-
-    # Dictionary to map Kickstarter column names to Indiegogo column names
-    kickstarter_to_indiegogo_mapping = {
-        'Name': 'title',
-        'Goal': 'goal_usd',
-        'Pledged': 'amount_raised_usd'
-        # Add more mappings as needed
-    }
-
-    # Rename columns in Kickstarter dataset
-    kickstarter_data.rename(columns=kickstarter_to_indiegogo_mapping, inplace=True)
-
-    return indiegogo_data, kickstarter_data
-
-
-def merge_datasets(kickstarter, indiegogo):
-    """
-    Merge the Kickstarter and Indiegogo datasets.
-    
-    Parameters:
-    - kickstarter (DataFrame): DataFrame containing the Kickstarter dataset.
-    - indiegogo (DataFrame): DataFrame containing the Indiegogo dataset.
-    
-    Returns:
-    - DataFrame: Merged dataset.
-    """
-    # Align column names between the datasets
-    align_column_names(indiegogo, kickstarter)
-
-    # Concatenate the datasets
-    merged_data = pd.concat([kickstarter, indiegogo], ignore_index=True)
-    return merged_data
+    return cleaned_df
 
 if __name__ == '__main__':
-    # Load the two datasets
-    kickstarter_data = pd.read_csv("../data/kickstarter_projects.csv")
-    indiegogo_data = pd.read_csv("../data/indiegogo.csv")
+    # If a folder to store preprocessed data does not exist, create it
+    path = "../data/preprocessed"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+    # Preprocess the kickstarter dataset and save it as a CSV file
+    kickstarter = pd.read_csv("../data/raw/kickstarter.csv")
+    kickstarter_preprocessed = preprocess_kickstarter(kickstarter)
+    kickstarter_preprocessed.to_csv('../data/preprocessed/kickstarter_preprocessed.csv', index=False)
 
-    # Preprocess Kickstarter data
-    kickstarter_data = preprocess_kickstarter_data(kickstarter_data)
-
-    # Preprocess Indiegogo data
-    indiegogo_data = preprocess_indiegogo_data(indiegogo_data)
-
-    # Merge the preprocessed datasets
-    merged_data = merge_datasets(kickstarter_data, indiegogo_data)
-
-    # Save the merged dataset to a CSV file
-    merged_data.to_csv('../data/merged_dataset.csv', index=False)
+    # Preprocess the indiegogo dataset and save it as a CSV file
+    indiegogo = pd.read_csv("../data/raw/indiegogo.csv")
+    indiegogo_preprocessed = preprocess_indiegogo(indiegogo)
+    indiegogo_preprocessed.to_csv('../data/preprocessed/indiegogo_preprocessed.csv', index=False)
